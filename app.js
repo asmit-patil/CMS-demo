c5cf43ec595719204e644c91d5bd20654a269fe3
asmit patil <patilasmit0@gmail.com>
2018-06-05 15:09:32 +0530

    removed repeated code

--- app.js
+++ app.js
@@ -8,22 +8,22 @@ app.set('view engine', 'html')
 var bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
-var Promise = require('bluebird');
-var request = Promise.promisifyAll(require('request'), {multiArgs: true});
+//var Promise = require('bluebird');
+var request = require('request')
 
 nunjucks.configure('views', {
     autoescape: false,
     express   : app
   });
 
-var urlList=[];
-function getBodies(array) {
-    return Promise.map(urlList, function(url) {
-        return request.getAsync(url).spread(function(response,body) {
-            return JSON.parse(body);
-        });
-    });
-}
+// var urlList=[];
+// function getBodies(array) {
+//     return Promise.map(urlList, function(url) {
+//         return request.getAsync(url).spread(function(response,body) {
+//             return JSON.parse(body);
+//         });
+//     });
+// }
 var urlstart ="https://cdn.contentstack.io/v3/content_types/"
 var urlend="/entries?api_key=bltd1343376dfba54d2&access_token=bltfe57b09b1e4c5732&environment=staging&locale=en-us&include_dimension=true"
 var headerurl= urlstart+"header"+urlend
@@ -32,63 +32,27 @@ var footerurl= urlstart+"footer"+urlend
 var faqurl= urlstart+"faq"+urlend
 var contactusurl =urlstart+"contact_us"+urlend
 
-app.get('/',function(req,res,next){
+var pf=function (url){
 
-    var getHeader = new Promise(function(resolve, reject) {
-       
-        console.log(headerurl)
+   return new Promise(function(resolve, reject) {
         request.get( options = {  
-            url: headerurl,
-           
-            method: 'GET'
-            
+            url: url,
+            method: 'GET'  
         }, function(err, resp, body) {
-          
             if (err) {
-                console.log(1)
-                reject(err);
+               return reject(err);
             } else {
-                resolve(JSON.parse(body));
+                return resolve(JSON.parse(body));
             }
         })
         
     })
-    
-    var getBody= new Promise(function( resolve,reject){
-        
-        console.log(homeurl)
-        request.get( options = {  
-            url:homeurl, 
-            
-            method: 'GET'
-            
-        },function(err, resp,body){
-            if (err) {
-                console.log(2)
-                reject(err);
-            } else {
-                resolve(JSON.parse(body));
-            }
-        })
-    })
+}
 
-    var getFooter = new Promise(function( resolve,reject){
-        
-        console.log(footerurl)
-        request.get( options = {  
-            url: footerurl,
-           
-            method: 'GET'
-            
-        },function(err, resp,body){
-            if (err) {
-                console.log(3)
-                reject(err);
-            } else {
-                resolve(JSON.parse(body));
-            }
-        })
-    })
+app.get('/',function(req,res,next){
+    getHeader=pf(headerurl)
+    getBody=pf(homeurl)
+    getFooter=pf(footerurl)
     Promise.all([getHeader,getBody,getFooter])
         .then(function(values) 
         {
@@ -99,51 +63,54 @@ app.get('/',function(req,res,next){
         })
         .catch(function(err){
             console.log(err)
-        })
-    // promiseFunction.then(function (result){
-    //     console.log(result)
-    //     res.render('home',{
-    //         data:result
-    //     })
-    // }).catch(function(err){
-    //     console.log(err)
-    // })
-    
-    
+        })   
 })
 
 
 app.get('/faq',function(req,res,next){
-
-urlList=[headerurl,faqurl,footerurl]
-console.log(urlList)
-    getBodies(urlList).then(function(results) {
-        
-        console.log(results)
-       
-        res.render('faq',{
-            data:results
+    getHeader=pf(headerurl)
+    getBody=pf(faqurl)
+    getFooter=pf(footerurl)
+    Promise.all([getHeader,getBody,getFooter])
+        .then(function(values) 
+        {
+            console.log(values);
+            res.render('faq',{
+                    data:values
+            })
         })
-        
-    }).catch(function(err) {
-        console.log(err)
-    });
+        .catch(function(err){
+            console.log(err)
+        })
 })
 
   
 app.get('/contactus',function(req,res,next){
     
-   urlList=[headerurl,contactusurl,footerurl]
-    getBodies(urlList).then(function(results) {
-        console.log(results)
-        res.render('contactus',{
-            data:results
-        })
+//    urlList=[headerurl,contactusurl,footerurl]
+//     getBodies(urlList).then(function(results) {
+//         console.log(results)
+//         res.render('contactus',{
+//             data:results
+//         })
         
-    }).catch(function(err) {
-        console.log(err)
-    });
-
+//     }).catch(function(err) {
+//         console.log(err)
+//     });
+    getHeader=pf(headerurl)
+    getBody=pf(contactusurl)
+    getFooter=pf(footerurl)
+    Promise.all([getHeader,getBody,getFooter])
+        .then(function(values) 
+        {
+            console.log(values);
+            res.render('contactus',{
+                    data:values
+            })
+        })
+        .catch(function(err){
+            console.log(err)
+        })
 
 })
 app.listen(port, function(){
